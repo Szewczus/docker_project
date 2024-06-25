05d2461a05ad6632f753466ec2681482ed9ec4c0
Ewus <ewus9999@gmail.com>
2024-06-25 15:06:53 +0200

    add test

---
+++ src/test/java/com/example/docker_project/restcontrollers/UserControllerTest.java
@@ -0,0 +1,47 @@
+package com.example.docker_project.restcontrollers;
+
+import com.example.docker_project.dtos.UserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("h2")
+@SpringBootTest
+@Sql("/databases/create_user.sql")
+class UserControllerTest {
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    @Test
+    void register() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setLogin("test1");
+        userDto.setName("Test");
+        userDto.setEmail("test@gmail.com");
+        userDto.setPassword("test");
+        userDto.setSurname("Test");
+        this.mockMvc.perform(post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDto)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
---
+++ src/test/resources/databases/create_user.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS user_entity CASCADE;
+CREATE TABLE user_entity(
+    id INTEGER PRIMARY KEY,
+    email VARCHAR(32),
+    login VARCHAR(32) UNIQUE,
+    name VARCHAR(32),
+    password VARCHAR(32),
+    surname VARCHAR(32),
+    notification_on BOOLEAN
+);
+INSERT INTO user_entity VALUES (1, 'ewus999@gmail.com', 'ewa', 'Ewa', 'test', 'Wieleba', true);
