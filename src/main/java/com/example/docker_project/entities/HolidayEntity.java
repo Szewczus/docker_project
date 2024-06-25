a2f547907c699f2c35cc060e93d326f897d4939e
Ewus <ewus9999@gmail.com>
2024-06-25 22:07:01 +0200

    remove unnecessary getters

--- src/main/java/com/example/docker_project/entities/HolidayEntity.java
+++ src/main/java/com/example/docker_project/entities/HolidayEntity.java
@@ -9,7 +9,6 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
-@Getter
 @Table(name = "holiday_entity")
 public class HolidayEntity {
     @Id
@@ -30,4 +29,59 @@ public class HolidayEntity {
     @OneToMany(mappedBy = "holiday", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Set<TripPlanEntity> tripPlans;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getArrive_date() {
+        return arrive_date;
+    }
+
+    public void setArrive_date(String arrive_date) {
+        this.arrive_date = arrive_date;
+    }
+
+    public String getDeparture_date() {
+        return departure_date;
+    }
+
+    public void setDeparture_date(String departure_date) {
+        this.departure_date = departure_date;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getAccomodation_cost() {
+        return accomodation_cost;
+    }
+
+    public void setAccomodation_cost(Double accomodation_cost) {
+        this.accomodation_cost = accomodation_cost;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
 }
